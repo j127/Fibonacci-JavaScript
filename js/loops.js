8dfc7caa67be9c5c842f31c470f514caf9ce851a
J. Cohen <hostelmarketing@gmail.com>
2013-10-17 13:23:57 -0700

    Add user input and CSS styling.
    Image is non-free. This is just a personal demo.

--- js/loops.js
+++ js/loops.js
@@ -1,9 +1,32 @@
 $(document).ready(function() {
+    var getFibCount = function (question) {
+        var minNumber = 1,
+            maxNumber = 100;
+
+        // TODO: fix global variable?
+        fibCount = window.prompt(question);
+
+        // Validate input
+        if((!isNumber(fibCount)) || (fibCount < 3) || (fibCount > 100)) {
+            getFibCount("You must enter a number between 1 and 100.");
+        }
+        return fibCount;
+    }
+
+    // Initial prompt window
+    getFibCount("How many fibs? (between 3 and 100)");
+
+    // Function to validate number
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+
+    // For loop version
     (function() {
         var forFib = [0, 1],
             current;
 
-        for (var i = 1; i  <= 8; i ++) {
+        for (var i = 1; i  <= fibCount - 2; i ++) {
             current = forFib[i] + forFib[i - 1];
             forFib.push(current);
         }
@@ -15,12 +38,13 @@ $(document).ready(function() {
         $('#forFibs').append(forFib);
     })();
 
+    // While loop version
     (function() {
         var whileFib = [0, 1],
-        current,
-        i = 1;
+            current,
+            i = 1;
 
-        while(i < 9) {
+        while(i <= fibCount - 2) {
             current = whileFib[i] + whileFib[i - 1];
             whileFib.push(current);
             i ++;

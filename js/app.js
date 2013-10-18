6848795188a24fb5112c26967a7e0600ffeff434
J. Cohen <hostelmarketing@gmail.com>
2013-10-18 11:36:37 -0700

    Rearrange file to comply with JSLint

--- js/app.js
+++ js/app.js
@@ -1,28 +1,62 @@
-(function() {
+/*jslint browser:true */
+/*jslint devel: true */
+/*jslint plusplus: true */
+(function () {
+    "use strict";
+
+    var loader = document.getElementById('loader'),
+        minFibNumber = 1,
+        maxFibNumber = 30,
+        min = document.getElementById('minNumber'),
+        max = document.getElementById('maxNumber'),
+        fibBtn = document.getElementById('printFibValues');
 
     // Show the content after the page loads
-    var loader = document.getElementById('loader');
     loader.style.display = 'block';
 
-    var minFibNumber = 1,
-        maxFibNumber = 30;
-
     // print the min and max to the HTML page template areas
-    min = document.getElementById('minNumber');
     min.innerHTML = minFibNumber;
     min.style.color = 'black';
 
-    max = document.getElementById('maxNumber');
     max.innerHTML = maxFibNumber;
     max.style.color = 'black';
 
-    // Listen for button click
-    var fibBtn = document.getElementById('printFibValues');
-    fibBtn.addEventListener('click', function(e) {
-        // Prevent form submission
-        e.preventDefault();
-        btnClick();
-    });
+    // Get input from user
+    function getFibOptions() {
+        var radioButtonsValid = false,
+            inputFieldValid = false,
+            fibCount = document.getElementById('numberOfValues').value,
+            format = document.getElementsByName('outputFormat'), // an array
+            printFormat,
+            i,
+            len;
+
+        // Validate input field
+        if ((isNaN(fibCount)) || (fibCount < minFibNumber) || (fibCount > maxFibNumber)) {
+            document.getElementById('message').innerHTML += 'You must enter a number between ' + minFibNumber + ' and ' + maxFibNumber + '.<br>';
+        } else {
+            inputFieldValid = true;
+        }
+
+        // Validate radio buttons - loop through the array using cached length
+        for (i = 0, len = format.length; i < len; i++) {
+            if (format[i].checked) {
+                radioButtonsValid = true;
+                console.log('radioButtonsValid = ' + radioButtonsValid);
+                printFormat = format[i].value;
+                console.log('Format: ' + format[i].value);
+            }
+        }
+            
+        if (radioButtonsValid === false) {
+            document.getElementById('message').innerHTML += 'You must choose a format.<br>';
+        }
+
+        // Generate the output
+        if ((radioButtonsValid === true) && (inputFieldValid === true)) {
+            generateFibs(fibCount, printFormat);
+        }
+    }
 
     function btnClick() {
         console.log('Button clicked');
@@ -35,71 +69,32 @@
         getFibOptions();
         console.log('-------');
 
-    };
-
-    // Get input from user
-    function getFibOptions() {
-        var radioButtonsValid = false,
-            inputFieldValid = false;
-
-        var fibCount = document.getElementById('numberOfValues').value;
-        var format = document.getElementsByName('outputFormat'); // an array
-
-        // Validate input field
-        if((isNaN(fibCount)) || (fibCount < minFibNumber) || (fibCount > maxFibNumber)) {
-            document.getElementById('message').innerHTML += 'You must enter a number between ' + minFibNumber + ' and ' + maxFibNumber + '.<br>';
-        } else {
-            inputFieldValid = true;
-        };
-
-        // Validate radio buttons - loop through the array using cached length
-        for(var i = 0, len = format.length; i < len; i ++) {
-            if(format[i].checked) {
-                radioButtonsValid = true;
-                console.log('radioButtonsValid = ' + radioButtonsValid);
-                printFormat = format[i].value;
-                console.log('Format: ' + format[i].value);
-            };
-        };
-
-        if(radioButtonsValid === false) {
-            document.getElementById('message').innerHTML += 'You must choose a format.<br>';
-        };
-
-        // Generate the output
-        if((radioButtonsValid === true) && (inputFieldValid === true)) {
-            generateFibs(fibCount, printFormat);
-        };
     }
 
-    // Generate and print data to screen
-    function generateFibs(num, format) {
-        // Generate the fibs
-        var forFib = [0],
-            current;
+    // Print a table
+    function fibsToTable(fibs) {
+        var fibsLen = fibs.length,
+            tableStart = '<table>',
+            tableEnd = '</table>',
+            tableMiddle = '',
+            i,
+            j,
+            counter;
 
-        if(num > 1) {
-            forFib.push(1);
-        };
-
-        for (var i = 1; i  <= num - 2; i ++) {
-            current = forFib[i] + forFib[i - 1];
-            forFib.push(current);
+        // Assemble table header
+        tableMiddle += '<tr>';
+        for (i = 0; i < fibsLen; i++) {
+            counter = i + 1;
+            tableMiddle += '<th>F' + counter + '</th>';
         }
-
-        console.log('Length of `for` array: ' + forFib.length);
-        console.log('forFib: ' + forFib);
-        fibArray = forFib;
-
-        // Check which format to output based on the input
-        if(format === 'console') {
-            fibsToConsole(fibArray);
-        } else if(format === 'table') {
-            fibsToTable(fibArray);
-        } else {
-            document.getElementById('message').innerHTML = 'There was an error with the formatting options.';
-        };
-    };
+        tableMiddle += '</tr><tr>';
+        // Assemble table body
+        for (j = 0; j < fibsLen; j++) {
+            tableMiddle += '<td>' + fibs[j] + '</td>';
+        }
+        tableMiddle += '</tr>';
+        document.getElementById('outputArea').innerHTML = '<h2>Your Fibs Table</h2>' + tableStart + tableMiddle + tableEnd;
+    }
 
     // Print to console / screen
     function fibsToConsole(fibs) {
@@ -110,29 +105,42 @@
         // Send output to screen
         fibs = fibs.join(', ');
         document.getElementById('outputArea').innerHTML = '<h2>Your Fibs String:</h2>' + fibs;
-    };
+    }
 
-    // Print a table
-    function fibsToTable(fibs) {
-        var fibsLen = fibs.length;
-        var tableStart = '<table>';
-        var tableEnd = '</table>';
-        var tableMiddle = '';
+    // Generate and print data to screen
+    function generateFibs(num, format) {
+        // Generate the fibs
+        var fibArray = [0],
+            current,
+            i;
 
-        // Assemble table header
-        tableMiddle += '<tr>';
-        for(var i = 0; i < fibsLen; i ++) {
-            counter = i + 1;
-            tableMiddle += '<th>F' + counter + '</th>';
-        };
-        tableMiddle += '</tr><tr>';
-        // Assemble table body
-        for(var i = 0; i < fibsLen; i ++) {
-            tableMiddle += '<td>' + fibs[i] + '</td>';
-        };
-        tableMiddle += '</tr>';
-        document.getElementById('outputArea').innerHTML = '<h2>Your Fibs Table</h2>' + tableStart + tableMiddle + tableEnd;
-    };
+        if (num > 1) {
+            fibArray.push(1);
+        }
 
-})();
+        for (i = 1; i <= num - 2; i++) {
+            current = fibArray[i] + fibArray[i - 1];
+            fibArray.push(current);
+        }
+
+        console.log('Length of `for` array: ' + fibArray.length);
+        console.log('fibArray: ' + fibArray);
+
+        // Check which format to output based on the input
+        if (format === 'console') {
+            fibsToConsole(fibArray);
+        } else if (format === 'table') {
+            fibsToTable(fibArray);
+        } else {
+            document.getElementById('message').innerHTML = 'There was an error with the formatting options.';
+        }
+    }
+
+    // Listen for button click
+    fibBtn.addEventListener('click', function (e) {
+        // Prevent form submission
+        e.preventDefault();
+        btnClick();
+    });
+}());
 
